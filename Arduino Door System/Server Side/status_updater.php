f3d49b100399afe9395973cf7feb0434be56cb3f
xОскарКоды <34571767+xOscarCodes@users.noreply.github.com>
2022-12-08 02:36:46 +0530

    Update status_updater.php

--- Arduino Door System/Server Side/status_updater.php
+++ Arduino Door System/Server Side/status_updater.php
@@ -1,23 +1,23 @@
 <?php
 $servername = "localhost";
 $dBUsername = "root";
-$dBPassword = "";
-$dBName = "locks";
+$dBPassword = "esdproject";
+$dBName = "door_data";
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
-}
-
+} 
 //Read the database
 if (isset($_POST['check_lock_status'])) {
 	$led_id = $_POST['check_lock_status'];	
-	$sql = "SELECT * FROM locks_status WHERE id = '$lock_id';";
+	$sql = "SELECT * FROM `lock_unlock_door` WHERE lock_id = '$led_id'";
 	$result   = mysqli_query($conn, $sql);
 	$row  = mysqli_fetch_assoc($result);
-	if($row['status'] == 0){
+	if($row['lock_status'] == 1){
 		echo "LOCK";
 	}
-	else{
+	else {
 		echo "UNLOCK";
 	}	
-}	
+}
+?>
