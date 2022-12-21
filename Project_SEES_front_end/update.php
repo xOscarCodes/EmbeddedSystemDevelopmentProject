2ca3fa1f9476b53b8ea426b38c1400fc354e89c7
harshdeep singh <106699976+harshdeep-s@users.noreply.github.com>
2022-12-21 11:05:48 +0530

    Delete Project_SEES_front_end directory

--- Project_SEES_front_end/update.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$conn = mysqli_connect("localhost", "root", "seesproject", "payment");
-
-$delete = "UPDATE pi_to_html SET Customer_ID='0'";
-
-mysqli_query($conn, $delete);
-mysqli_close($conn);
-?>
